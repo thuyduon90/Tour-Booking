7515d7302112d38f2d01818fb7ea31059aae0d12
DuongThuy <thuydv1111@gmail.com>
2020-04-28 10:00:06 +0900

    Add Aggreagation Middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -134,9 +134,9 @@ exports.getTourStats = async(req, res) => {
             {
                 $sort: { avgPrice: 1 },
             },
-            {
-                $match: { _id: { $ne: 'EASY' } },
-            },
+            // {
+            //     $match: { _id: { $ne: 'EASY' } },
+            // },
         ]);
         res.status(200).json({
             status: 'success',
