b06e31d8d264c13850ea01b651d50878ff56a73c
DuongThuy <thuydv1111@gmail.com>
2020-05-01 22:17:00 +0900

    Encrypt Password

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -34,7 +35,21 @@ const userSchema = new mongoose.Schema({
     passwordConfirm: {
         type: String,
         required: [true, 'Password need to be confirmed'],
+        validate: {
+            // this only work on CREATE or SAVE
+            validator: function(el) {
+                return el === this.password;
+            },
+            message: 'Not a simmilar password!',
+        },
     },
+});
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordConfirm = undefined;
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
