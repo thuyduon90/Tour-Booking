91dbbf0186087cb4af4e98469d03e1e990521c94
DuongThuy <thuydv1111@gmail.com>
2020-05-07 11:37:09 +0900

    Reference guides in tourModel from User model

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
 
-const User = require('./userModel');
+// const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -110,7 +110,10 @@ const tourSchema = new mongoose.Schema({
         description: String,
         day: Number,
     }, ],
-    guides: Array,
+    guides: [{
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+    }, ],
 }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
@@ -127,13 +130,13 @@ tourSchema.pre('save', function(next) {
     next();
 });
 
-tourSchema.pre('save', async function(next) {
+/* tourSchema.pre('save', async function(next) {
     const guidesPromises = this.guides.map(
         async id => await User.findById(id)
     );
     this.guides = await Promise.all(guidesPromises);
     next();
-});
+}); */
 
 /* tourSchema.post('save', function(doc, next) {
     console.log(doc);
