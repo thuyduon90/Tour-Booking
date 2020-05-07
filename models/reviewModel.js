e1744f476a20c9586216152b9416562f047373f0
DuongThuy <thuydv1111@gmail.com>
2020-05-07 15:08:24 +0900

    Create reviewModel

---
+++ models/reviewModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+// const slugify = require('slugify');
+// const validator = require('validator');
+
+const reviewSchema = new mongoose.Schema({
+    review: {
+        type: String,
+        required: [true, 'A review must need content.'],
+        trim: true,
+        maxlength: [
+            1000,
+            'A review must be shorter 1000 characters',
+        ],
+    },
+    rating: {
+        type: Number,
+        default: 1,
+        min: [1, 'Rating must be above 1.0'],
+        max: [5, 'Rating must be below 5.0'],
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    tour: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Tour',
+        required: [true, 'A review must belong to a tour.'],
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: [true, 'A review must belong to a user.'],
+    },
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
