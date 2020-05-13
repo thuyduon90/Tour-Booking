a0c5d0adddfc78cb18a42da73563ff85d12d2cd2
DuongThuy <thuydv1111@gmail.com>
2020-05-13 14:00:26 +0900

    Show tour list to overview page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,13 @@
-exports.getOverview = (req, res) => {
+const Tour = require('./../models/tourModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getOverview = catchAsync(async(req, res) => {
+    const tours = await Tour.find();
     res.status(200).render('overview', {
-        title: 'All Tours'
+        title: 'All Tours',
+        tours
     });
-};
+});
 
 exports.getTour = (req, res) => {
     res.status(200).render('tour', {
--- routes/viewRoute.js
+++ routes/viewRoute.js
@@ -3,7 +3,7 @@ const viewRouter = express.Router();
 
 const viewController = require('./../controllers/viewController');
 
-viewRouter.get('/overview', viewController.getOverview);
+viewRouter.get('/', viewController.getOverview);
 viewRouter.get('/tour', viewController.getTour);
 
 module.exports = viewRouter;
