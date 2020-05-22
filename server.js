6748dcc78e4512a86cbaf51555d6f3493cf77384
DuongThuy <thuydv1111@gmail.com>
2020-05-22 18:00:01 +0900

    fix bug 1

--- server.js
+++ server.js
@@ -4,25 +4,24 @@ const dotenv = require('dotenv');
 process.on('uncaughtException', err => {
     console.log(err.name);
     console.log(err.message);
-    console.log(
-        'UNCAUGHT EXCEPTION 🎆. Now, server will shutdown...'
-    );
+    console.log('UNCAUGHT EXCEPTION 🎆. Now, server will shutdown...');
     process.exit(1);
 });
 
 dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE.replace(
-    '<PASSWORD>',
-    process.env.DATABASE_PASSWORD
-);
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+console.log('here--------------------------');
+
+console.log(DB);
 
 mongoose
     .connect(DB, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
-        useUnifiedTopology: true,
+        useUnifiedTopology: true
     })
     .then(() => console.log('DB connected!'));
 
@@ -37,9 +36,7 @@ const server = app.listen(port, () => {
 process.on('unhandledRejection', err => {
     console.log(err.name);
     console.log(err.message);
-    console.log(
-        'UNHANDLED REJECTION 🎆. Now, server will shutdown...'
-    );
+    console.log('UNHANDLED REJECTION 🎆. Now, server will shutdown...');
     server.close(() => {
         process.exit(1);
     });
