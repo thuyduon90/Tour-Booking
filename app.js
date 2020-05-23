ae5e6bd22a9e5b5a9e7029d4a9ca0297e3bfa580
DuongThuy <thuydv1111@gmail.com>
2020-05-23 10:02:43 +0900

    Test for secure HTTPS connection, Responding to SIGTERM sinal, Implementing CORS

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +27,10 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 /* GLOBAL MIDDLEWARES */
+// implementing CORS
+app.use(cors());
+app.options('*', cors());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
