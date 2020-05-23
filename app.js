dee87431f200e520bb02f74ab6a128a6a055a63d
DuongThuy <thuydv1111@gmail.com>
2020-05-23 12:50:38 +0900

    Apply Stripe webhook

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const compression = require('compression');
 const cors = require('cors');
 
@@ -53,7 +54,7 @@ app.use('/api', limiter);
 
 app.post(
     '/webhook-checkout',
-    express.raw({ type: 'application/json' }),
+    bodyParser.raw({ type: 'application/json' }),
     bookingController.webhookCheckout
 );
 
