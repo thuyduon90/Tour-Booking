326c4f344991486ca07a6e3d7d2b1c05bd0ddb3c
DuongThuy <thuydv1111@gmail.com>
2020-05-23 13:53:36 +0900

    Apply Stripe webhook 3

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -26,7 +26,7 @@ exports.getCheckoutSession = catchAsync(async(req, res, next) => {
         line_items: [{
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+            images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
             amount: tour.price * 100,
             currency: 'usd',
             quantity: 1
@@ -54,7 +54,7 @@ exports.getCheckoutSession = catchAsync(async(req, res, next) => {
 const createBookingCheckout = async session => {
     const tour = session.client_reference_id;
     const user = (await User.findOne({ email: session.customer_email })).id;
-    const price = session.line_items[0].amount / 100;
+    const price = session.display_items[0].amount / 100;
     await Booking.create({ tour, user, price });
 };
 
